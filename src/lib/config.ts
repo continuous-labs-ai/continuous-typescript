9c5243c8c20001351859584e9052028d393fdb46
Minh Pham <288481663+minh-continuous@users.noreply.github.com>
2026-09-18 02:54:13 +0000

    ci: regenerated with OpenAPI Doc , Speakeasy CLI 1.796.4 (#13)
    Co-authored-by: speakeasybot <bot@speakeasyapi.dev>

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -61,7 +61,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "v1",
-  sdkVersion: "0.0.12",
+  sdkVersion: "0.0.13",
   genVersion: "2.935.1",
-  userAgent: "speakeasy-sdk/typescript 0.0.12 2.935.1 v1 @continuous-labs/sdk",
+  userAgent: "speakeasy-sdk/typescript 0.0.13 2.935.1 v1 @continuous-labs/sdk",
 } as const;
